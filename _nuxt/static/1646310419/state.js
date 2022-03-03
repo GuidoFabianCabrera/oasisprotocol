2b741415bbe3300bef3495f6d5e1aef1f9acc787
GuidoFC <guidofabiancabrera@hotmail.com>
2022-03-03 09:27:07 -0300

    8582e90

---
+++ _nuxt/static/1646310419/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Foasisprotocol\u002F_nuxt\u002Fstatic\u002F1646310419",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Foasisprotocol\u002F",assetsPath:"\u002Foasisprotocol\u002F_nuxt\u002F",cdnURL:a}}}}(null));
